f0e0d90f876f0c8d045533ac66565420f648e673
motemen <motemen@gmail.com>
2016-02-01 20:24:08 +0900

    earn coverage

---
+++ src/test/scala/prchecklist/models/GitHubTypesSpec.scala
@@ -0,0 +1,17 @@
+package prchecklist.models
+
+import GitHubTypes._
+import prchecklist.test.Factory
+
+import org.scalatest._
+
+class GitHubTypesSpec extends FunSuite with Matchers {
+  test("PullRequest#isOpen/isClosed") {
+    val pr = Factory.createGitHubPullRequest
+    pr.copy(state = "open").isOpen shouldBe true
+    pr.copy(state = "closed").isOpen shouldBe false
+
+    pr.copy(state = "open").isClosed shouldBe false
+    pr.copy(state = "closed").isClosed shouldBe true
+  }
+}
