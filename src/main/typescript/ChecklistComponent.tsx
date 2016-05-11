5e6d41b8f72d4e109820fbcf311b920b18bc5ee5
motemen <motemen@gmail.com>
2016-05-11 21:27:48 +0900

    wip: react

---
+++ src/main/typescript/ChecklistComponent.tsx
@@ -0,0 +1,136 @@
+import * as React from 'react';
+import {Checkbox,Paper,List,ListItem,CircularProgress,Avatar} from 'material-ui';
+
+// prchecklist.views.Checklist
+interface Checklist {
+  repo:        Repo;
+  pullRequest: PullRequest;
+  stage:       string;
+  checks:      Check[];
+}
+
+interface Repo {
+  fullName: string;
+}
+
+interface PullRequest {
+  url:    string;
+  number: number;
+  title:  string;
+  body:   string;
+}
+
+interface Check {
+  url:     string;
+  number:  number;
+  title:   string;
+  users:   User[];
+  checked: boolean;
+}
+
+interface User {
+  name:      string;
+  avatarUrl: string;
+}
+
+interface ChecklistComponentProps {
+  repoOwner:         string;
+  repoName:          string;
+  pullRequestNumber: number;
+}
+
+interface ChecklistComponentState {
+  checklist: Checklist;
+  checkLoading: { [i: number]: boolean; };
+}
+
+module API {
+  function updateChecklist(mode: string, checklist: Checklist, featureNumber: number): Promise<Checklist> {
+    const [repoOwner, repoName] = checklist.repo.fullName.split('/');
+    let body = [
+      `repoOwner=${repoOwner}`,
+      `repoName=${repoName}`,
+      `pullRequestNumber=${checklist.pullRequest.number}`,
+      `stage=${checklist.stage}`,
+      `featureNumber=${featureNumber}`
+    ].join('&');
+    return fetch(`/-/checklist/${mode}`, { method: 'POST', credentials: 'same-origin', headers: { 'Content-Type': 'application/x-www-form-urlencoded' }, body: body }).then(
+      res => res.json<Checklist>()
+    );
+  }
+
+  export function checkChecklist(checklist: Checklist, featureNumber: number): Promise<Checklist> {
+    return updateChecklist('check', checklist, featureNumber);
+  }
+
+  export function uncheckChecklist(checklist: Checklist, featureNumber: number): Promise<Checklist> {
+    return updateChecklist('uncheck', checklist, featureNumber);
+  }
+
+  export function fetchChecklist(repoOwner: string, repoName: string, pullRequestNumber: number, stage: string = ""): Promise<Checklist> {
+    return fetch(`/-/checklist?repoOwner=${repoOwner}&repoName=${repoName}&pullRequestNumber=${pullRequestNumber}&stage=${stage}`, { credentials: 'same-origin' }).then(
+      res => res.json<Checklist>()
+    );
+  }
+}
+
+export const ChecklistComponent = React.createClass<ChecklistComponentProps, ChecklistComponentState>({
+  _handleCheck(check: Check, i: number) {
+    return (e, isChecked) => {
+      let updateChecklist = isChecked ? API.uncheckChecklist : API.checkChecklist;
+      updateChecklist(this.state.checklist, check.number).then(checklist => {
+        this.setState({ checklist: checklist })
+      });
+    };
+  },
+
+  componentWillMount() {
+    const props: ChecklistComponentProps = this.props;
+    API.fetchChecklist(props.repoOwner, props.repoName, props.pullRequestNumber)
+      .then((checklist) => {
+        this.setState({ checklist: checklist });
+      });
+  },
+
+  getInitialState(): ChecklistComponentState {
+    return {
+      checklist: null,
+      checkLoading: {}
+    };
+  },
+
+  render() {
+    if (!this.state.checklist) {
+      return (
+        <section>
+          <h2>
+            <small>{this.props.repoOwner}/{this.props.repoName} #{this.props.pullRequestNumber}</small>
+          </h2>
+          <div style={{ textAlign: 'center' }}><CircularProgress /></div>
+        </section>
+      );
+    }
+
+    return (
+      <section>
+        <h2>
+          <small>{this.props.repoOwner}/{this.props.repoName} #{this.props.pullRequestNumber}</small>
+        </h2>
+        <h1>
+          {this.state.checklist.pullRequest.title}
+        </h1>
+        <pre>{this.state.checklist.pullRequest.body}</pre>
+        <List>
+        {
+          this.state.checklist.checks.map((check: Check, i: number) => (
+            <ListItem leftCheckbox={<Checkbox disabled={this.state.checkLoading[i]} defaultChecked={check.checked} onCheck={this._handleCheck(check, i)} />} >
+                #{check.number} {check.title}
+                {check.users.map(user => <Avatar src={user.avatarUrl} size={16} style={{ verticalAlign: 'middle' }} />)}
+            </ListItem>
+          ))
+        }
+        </List>
+      </section>
+    );
+  }
+});
---
+++ src/main/typescript/app.tsx
@@ -0,0 +1,45 @@
+import * as React       from 'react';
+import * as ReactDOM    from 'react-dom';
+import { Router, Route, RouteComponentProps, Link, browserHistory }  from 'react-router'
+
+import {Styles} from 'material-ui';
+
+import {ChecklistComponent} from './ChecklistComponent';
+
+const IndexPage = React.createClass({
+  render() {
+    return (
+      <div>
+        <h2>For development</h2>
+        <ul>
+          <li><Link to={"/-/motemen/test/pull/3"}>motemen/test#3</Link></li>
+        </ul>
+      </div>
+    );
+  }
+});
+
+interface ChecklistPageParams {
+  repoOwner:         string;
+  repoName:          string;
+  pullRequestNumber: number;
+}
+
+const ChecklistPage = React.createClass<RouteComponentProps<{}, ChecklistPageParams>, {}>({
+  render() {
+    const params: ChecklistPageParams = this.props.params;
+    return (
+      <div style={{ margin: `${Styles.Spacing.desktopGutter * 3}px ${Styles.Spacing.desktopGutter * 4}px`, maxWidth: 768 }}>
+        <ChecklistComponent repoOwner={this.props.params.repoOwner} repoName={this.props.params.repoName} pullRequestNumber={this.props.params.pullRequestNumber} />
+      </div>
+    );
+  }
+});
+
+ReactDOM.render(
+  <Router history={browserHistory}>
+    <Route path="/-/" component={IndexPage} />
+    <Route path="/-/:repoOwner/:repoName/pull/:pullRequestNumber" component={ChecklistPage} />
+  </Router>,
+  document.querySelector('#app')
+);
