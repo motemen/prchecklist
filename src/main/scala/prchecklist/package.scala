3687c143ccdfd1d88180a051a4e9b7833fd2084a
motemen <motemen@gmail.com>
2017-04-11 23:04:53 +0900

    ws

--- src/main/scala/prchecklist/package.scala
+++ src/main/scala/prchecklist/package.scala
@@ -2,6 +2,5 @@ import org.json4s.native.{ Serialization => JsonSerialization }
 import org.json4s.NoTypeHints
 
 package object prchecklist {
-
   implicit def jsonFormats = JsonSerialization.formats(NoTypeHints)
 }
