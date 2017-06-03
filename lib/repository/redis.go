7ace0bca120f21a38e2c5e0b89528a7872de4b5e
motemen <motemen@gmail.com>
2017-06-03 23:35:06 +0900

    repo redis: fix

--- lib/repository/redis.go
+++ lib/repository/redis.go
@@ -42,7 +42,7 @@ func (r redisCoreRepository) conn() (redis.Conn, error) {
 			opts = append(opts, redis.DialPassword(pw))
 		}
 	}
-	return redis.Dial("tcp", r.url.Hostname(), opts...)
+	return redis.Dial("tcp", r.url.Host, opts...)
 }
 
 func (r redisCoreRepository) withConn(f func(redis.Conn) error) error {
@@ -71,6 +71,10 @@ func (r redisCoreRepository) AddUser(ctx context.Context, user prchecklist.GitHu
 
 func (r redisCoreRepository) GetUsers(ctx context.Context, userIDs []int) (map[int]prchecklist.GitHubUser, error) {
 	users := make(map[int]prchecklist.GitHubUser, len(userIDs))
+	if len(userIDs) == 0 {
+		return users, nil
+	}
+
 	err := r.withConn(func(conn redis.Conn) error {
 		keys := make([]interface{}, len(userIDs))
 		for i, id := range userIDs {
@@ -104,15 +108,13 @@ func (r redisCoreRepository) GetChecks(ctx context.Context, clRef prchecklist.Ch
 	err := r.withConn(func(conn redis.Conn) error {
 		key := redisKeyPrefixCheck + clRef.String()
 		buf, err := redis.Bytes(conn.Do("GET", key))
-		if err != nil && err != redis.ErrNil {
-			return err
-		}
-		err = json.Unmarshal(buf, &checks)
-		if err != nil {
+		if err == redis.ErrNil {
+			return nil
+		} else if err != nil {
 			return err
 		}
 
-		return nil
+		return json.Unmarshal(buf, &checks)
 	})
 
 	return checks, errors.Wrap(err, "GetChecks")
@@ -128,16 +130,15 @@ func (r redisCoreRepository) AddCheck(ctx context.Context, clRef prchecklist.Che
 
 		dbKey := redisKeyPrefixCheck + clRef.String()
 		buf, err := redis.Bytes(conn.Do("GET", dbKey))
-		if err != nil && err != redis.ErrNil {
-			return err
-		}
-		err = json.Unmarshal(buf, &checks)
-		if err != nil {
-			return err
-		}
-
-		if checks == nil {
+		if err == redis.ErrNil {
 			checks = prchecklist.Checks{}
+		} else if err != nil {
+			return err
+		} else {
+			err := json.Unmarshal(buf, &checks)
+			if err != nil {
+				return err
+			}
 		}
 
 		if checks.Add(key, user) == false {
@@ -164,16 +165,15 @@ func (r redisCoreRepository) RemoveCheck(ctx context.Context, clRef prchecklist.
 
 		dbKey := redisKeyPrefixCheck + clRef.String()
 		buf, err := redis.Bytes(conn.Do("GET", dbKey))
-		if err != nil && err != redis.ErrNil {
-			return err
-		}
-		err = json.Unmarshal(buf, &checks)
-		if err != nil {
-			return err
-		}
-
-		if checks == nil {
+		if err == redis.ErrNil {
 			checks = prchecklist.Checks{}
+		} else if err != nil {
+			return err
+		} else {
+			err := json.Unmarshal(buf, &checks)
+			if err != nil {
+				return err
+			}
 		}
 
 		if checks.Remove(key, user) == false {
